d48155479dcb32acbceafdbbb91b0f0187ec3f2a
JLans <lansford.jl@gmail.com>
2016-05-04 13:43:25 -0400

    altered XDATCAR to distances to takein coordinates

--- XDATCAR_2_distances.py
+++ XDATCAR_2_distances.py
@@ -10,7 +10,7 @@ import numpy as np
 import Text_Parse
 #os.listdir('C:/Users/Josh/Desktop/XSD files')
 # Run the above function and store its results in a variable.
-filename=os.path.expanduser('~\Box Sync\Synced_Files\Coding\Research\Analysis\O2_PW91_XDATCAR')
+filename=os.path.expanduser('~\Box Sync\Synced_Files\Coding\Research\Analysis\O2_PW91_XDATCAR_v02')
 #labels1212 = np.genfromtxt('./Lansford_Stats_HW3_data/12.12.csv', delimiter=',', dtype=str)[0,:]
 #data1212 = np.genfromtxt('C:\Users\Josh\Desktop\XSD files', delimiter=',')[1:,:]
 Surface_Distances = []
@@ -25,3 +25,10 @@ Coordinates = Text_Parse.list_split(Coordinates," ")
 Coordinates = Text_Parse.string2float(Coordinates)
 Coordinates = np.array(Coordinates)
 Coordinates = np.dot(Coordinates,Multiplier)
+trials = len(Coordinates[:,0])
+distance = np.zeros(int(trials/2))
+for i in range(0,trials,2):
+    distance[int(i/2)] = (sum((Coordinates[i,:]-Coordinates[i+1,:])**2))**0.5
+    
+print(distance)
+
